b1112c03ed005335f05a0a01e1408780322a71f1
Khokhlov Gregory <khokhlov.greg@gmail.com>
2020-02-22 21:46:32 +0300

    AudioComponent

---
+++ Assets/Source/Common/AudioSystem/AudioComponent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Common.AudioSystem
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioComponent : MonoBehaviour
+    {
+        private AudioSource _source;
+        public AudioTrack track;
+
+        private void Awake()
+        {
+            _source = GetComponent<AudioSource>();
+        }
+        public void Play()
+        {
+            track.Play(_source);            
+        }
+    }
+}
