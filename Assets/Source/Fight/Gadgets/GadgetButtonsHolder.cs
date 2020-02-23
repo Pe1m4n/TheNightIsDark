9d942e02f2efacca880871da811e04ee4cb945c2
Khokhlov Gregory <khokhlov.greg@gmail.com>
2020-02-23 12:17:39 +0300

    Not trying to build gadgets if they are missing

--- Assets/Source/Fight/Gadgets/GadgetButtonsHolder.cs
+++ Assets/Source/Fight/Gadgets/GadgetButtonsHolder.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Fight.State;
+using UnityEngine;
 using Zenject;
 
 namespace Fight.Gadgets
@@ -6,20 +7,30 @@ namespace Fight.Gadgets
     public class GadgetButtonsHolder : MonoBehaviour
     {
         private DayBehaviour _dayBehaviour;
+        private PlayerState _playerState;
         
         [Inject]
-        public void SetUp(DayBehaviour dayBehaviour)
+        public void SetUp(DayBehaviour dayBehaviour, PlayerState playerState)
         {
             _dayBehaviour = dayBehaviour;
+            _playerState = playerState;
         }
 
         public void TrySetBuildBarrel()
         {
+            if (_playerState.InventoryState.BarrelCount <= 0)
+            {
+                return;
+            }
             _dayBehaviour.State = DayBehaviour.DayState.BuildingBarrel;
         }
 
         public void TrySetBuildMine()
         {
+            if (_playerState.InventoryState.MineCount <= 0)
+            {
+                return;
+            }
             _dayBehaviour.State = DayBehaviour.DayState.BuildingMine;
         }
     }
